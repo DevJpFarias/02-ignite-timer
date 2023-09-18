14738faa16ae3f07655716b4c20cc9e8384e1ceb
João <joaopfc2764@gmail.com>
2023-09-18 14:31:27 -0300

    refactor: resolvendo problema na importação e aplicando css

--- src/layout/DefaultLayout/index.tsx
+++ src/layout/DefaultLayout/index.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from 'react-router-dom'
-import { Header } from '../components/Header'
+import { Header } from '../../components/Header'
+import { LayoutComponent } from './styles'
 
 export function DefaultLayout() {
   return (
-    <div>
+    <LayoutComponent>
       <Header />
       <Outlet />
-    </div>
+    </LayoutComponent>
   )
 }
