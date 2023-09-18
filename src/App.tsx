c24d01d49afbb2d5577134a17760e2d051fd521a
João <joaopfc2764@gmail.com>
2023-09-18 11:29:31 -0300

    refactor: aplicando router no projeto e apagando botões

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import { ThemeProvider } from "styled-components";
-import { Button } from "./components/Button";
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
 
-import { defaultTheme } from "./styles/themes/default";
-import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+
+import { Router } from './Router'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button />
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="success" />
-      <Button variant="danger" />
+      <BrowserRouter>
+        <Router />
 
-      <GlobalStyle />
+        <GlobalStyle />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
